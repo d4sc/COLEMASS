a0a97aacb2c19254a93530d1038f2d512c7ea980
d4sc <schaeferdaniel@fastmail.net>
2016-02-17 15:45:20 +0100

    Minor changes in HW app

--- webapp/hardware/views.py
+++ webapp/hardware/views.py
@@ -1,3 +1,118 @@
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+85
+86
+87
+88
+89
+90
+91
+92
+93
+94
+95
+96
+97
+98
+99
+100
+101
+102
+103
+104
+105
+106
+107
+108
+109
+110
+111
+112
+113
+
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import *
@@ -24,7 +139,7 @@ def gcl(request):
     s=''
     for c in cards:
         if len(s) > 0:
-            s+=','          
+            s+=','
         s+=c.key
     # s = '<%s>' % ','.join([di['key'] for di in cl])
     return HttpResponse('<%s>' % s)
@@ -41,6 +156,7 @@ def newHw(request):
         txt_hwid = request.POST.get('txt_hwid').strip()
         txt_psw = request.POST.get('txt_sn').strip()
         txt_title = request.POST.get('txt_title').strip()
+        hwtype_radio = request.POST.get('hwtype')
         import re
         reg = re.compile(r'^[\w\s]+$')
         if reg.search(txt_hwid) is None:
@@ -56,6 +172,7 @@ def newHw(request):
         from django.db import IntegrityError
         if hwtype_radio is not None:
             if hwtype_radio == '0':
+                from appliances.models import Appliance, ApplianceChore
                 try:
                     a = Appliance()
                     a.hwid = txt_hwid
@@ -63,14 +180,21 @@ def newHw(request):
                     a.name = txt_title
                     a.generatePad()
                     a.save()
+
+                    ac = ApplianceChore()
+                    ac.appliance =  a
+                    ac.name = a.name + " Chore"
+                    ac.save()
+
                 except IntegrityError:
                     messages.error(request, 'Appliance with this id already exists.')
-                    return render(request, 'hardware/newhw.html')  
+                    return render(request, 'hardware/newhw.html')
                 else:
                     messages.info(request, a.getName() + ' successfully registered.')
                     return redirect('hardware:hwlist')
 
             elif hwtype_radio == '1':
+                from dishes.models import ItemTracker
                 try:
                     h = ItemTracker()
                     h.hwid = txt_hwid
